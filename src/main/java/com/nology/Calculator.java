999680719101e14e24ec92223d11cb01456abef7
Gar-Yin Lam <garyin.lam@gmail.com>
2022-12-05 22:48:29 +0000

    create tests

--- src/main/java/com/nology/Calculator.java
+++ src/main/java/com/nology/Calculator.java
@@ -1,4 +1,31 @@
 package com.nology;
 
 public class Calculator {
+    public double add(double num1, double num2) {
+        return 0;
+    }
+
+    public double subtract(double num1, double num2) {
+        return 0;
+    }
+
+    public double multiply(double num1, double num2) {
+        return 0;
+    }
+
+    public double divide(double num1, double num2) {
+        return 0;
+    }
+
+    public double square(double num) {
+        return 0;
+    }
+
+    public double sqrt(double num) {
+        return 0;
+    }
+
+    public double evaluate(String s) {
+        return 0;
+    }
 }
